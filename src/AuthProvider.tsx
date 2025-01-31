6092f4adffbd09ee55955854c8764390d83cfce1
an1co <skye.anico@gmail.com>
2025-01-31 18:54:00 -0500

    added axios. created authprovider. created signin component.

---
+++ src/AuthProvider.tsx
@@ -0,0 +1,34 @@
+import { useContext, createContext, useState } from "react";
+
+interface ProviderProps {
+  user: string | null;
+  token: string;
+  login(data: LoginType): void;
+  logout(): void;
+}
+
+const AuthContext = createContext<ProviderProps>({
+  user: null,
+  token: "",
+  login: () => {},
+  logout: () => {},
+});
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [user, setUser] = useState(null);
+  return (
+    <AuthContext.Provider value={{ token, user, loginAction, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
